7e063ad0947cd3f0637ce50b6dab71cc87b4be31
Diego Gonzalez Chavez 5613 <u4h8@fresnou.org>
2023-09-06 08:38:55 -0700

    Finished Palindrome Checker

---
+++ routes/index.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index');
+});
+module.exports = router;
+
+router.post('/', function(req, res) {
+  res.render('index', {
+    phrase: req.body.userText,
+    message:getResultDescription(req.body.userText)
+  });
+});
+
+
+
+
+
+function checkPalidrone(phrase){
+  let temp = phrase.split("");
+  temp = temp.reverse();
+  temp = temp.join("")
+  if (phrase.toLowerCase().replace(" ", " ") === temp.toLowerCase().replace(" ", " ") ){
+    return true
+  }
+  else {
+    return false
+  }
+}
+
+function getResultDescription (phrase){
+  if (checkPalidrone(phrase)){
+    return `${phrase} is a palindrome.`
+  }
+  return `${phrase} is not a palindrome.`
+}
